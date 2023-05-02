dc72325bf1556b6bf43d8c62d211135441833f5d
alexmccray23 <alex.mccray@gmail.com>
2023-05-01 22:11:40 -0400

    Adding completed series.lua script

--- series.lua
+++ series.lua
@@ -1,46 +1,66 @@
--- Neovim plugin that performs an inital cleanup of the specs used for summary tables
-local wholeText = ""
+-- Neovim plugin that formats a question series in Uncle
+local count = 1
+local wholeText = {}
+local orig_cols = nil
+local new_cols = nil
 
+local function tableSeries()
+  local totTables = vim.fn.input("How many tables in series?\n")
+  if totTables == "" then
+    return
+  else
+    count = vim.fn.str2nr(totTables) - 1
+  end
 
-local function getFormat()
-  vim.fn.search('^TABLE', 'b')
-  vim.fn.search('^R &IN2BASE==', 'W')
-  local start_line = vim.fn.getcurpos()[2] + 1
-  vim.api.nvim_win_set_cursor(0, { start_line, 0 })
-  local end_line = vim.fn.search('\\*\\n', 'W')
-  local lines = vim.api.nvim_buf_get_lines(0, start_line - 1, end_line - 1, false)
-  wholeText = table.concat(lines, '\n')
-  return wholeText
+  local function getFormat()
+    vim.fn.search('^TABLE', 'b')
+    vim.fn.search('^R &IN2BASE==', 'W')
+    local start_line = vim.fn.getcurpos()[2] + 1
+    vim.api.nvim_win_set_cursor(0, { start_line, 0 })
+    local end_line = vim.fn.search('\\*\\n', 'W')
+    wholeText = vim.api.nvim_buf_get_lines(0, start_line - 1, end_line - 1, false)
+    return wholeText
+  end
+
+  local function getColumnsFirst()
+    vim.fn.search('^R &IN2BASE==', 'b')
+    vim.fn.search('^R \\w', 'W')
+    local orig_line = vim.api.nvim_get_current_line()
+    local pattern =
+    "\\(.\\+;\\)\\(R\\?(\\?1\\?\\!\\?\\|A\\?(\\?1\\?\\!\\?\\|PC\\?(\\?1\\?\\!\\?\\)\\(.\\+\\)\\(,\\|-\\)\\(.*\\)"
+    orig_cols = vim.fn.substitute(orig_line, pattern, "\\3", "")
+    return orig_cols
+  end
+
+  local function getColumnsRest()
+    vim.fn.search('^R &IN2BASE==', 'W')
+    vim.fn.search('^R \\w', 'W')
+    local new_line = vim.api.nvim_get_current_line()
+    local pattern =
+    "\\(.\\+;\\)\\(R\\?(\\?1\\?\\!\\?\\|A\\?(\\?1\\?\\!\\?\\|PC\\?(\\?1\\?\\!\\?\\)\\(.\\+\\)\\(,\\|-\\)\\(.*\\)"
+    new_cols = vim.fn.substitute(new_line, pattern, "\\3", "")
+    return new_cols
+  end
+
+  local function replaceOld()
+    local new_lines = {}
+    vim.fn.search('^TABLE', 'b')
+    vim.fn.search('^R &IN2BASE==', 'W')
+    local nline = vim.fn.getcurpos()[2] + 1
+    vim.api.nvim_win_set_cursor(0, { nline, 0 })
+    local eline = vim.fn.search('\\*\\n', 'W')
+    for index, line in ipairs(wholeText) do
+      new_lines[index] = vim.fn.substitute(line, orig_cols, new_cols, "")
+    end
+    vim.api.nvim_buf_set_lines(0, nline - 1, eline - 1, false, new_lines)
+  end
+
+  getFormat()
+  getColumnsFirst()
+
+  for _ = 1, count do
+    getColumnsRest()
+    replaceOld()
+  end
 end
-
-local function getColumnsFirst()
-  vim.fn.search('^R &IN2BASE==', 'W')
-  vim.fn.search('^R \\w', 'W')
-  local orig_line = vim.api.nvim_get_current_line()
-  local pattern = "\\(.\\+;\\)\\(R\\?(\\?1\\?\\!\\?\\|A\\?(\\?1\\?\\!\\?\\|PC\\?(\\?1\\?\\!\\?\\)\\(.\\+\\)\\(,\\|-\\)\\(.*\\)"
-  local orig_cols = vim.fn.substitute(orig_line, pattern, "\\3", "")
-  return orig_cols
-end
-
-local function getColumnsRest()
-  vim.fn.search('^R &IN2BASE==', 'W')
-  vim.fn.search('^R \\w', 'W')
-  local new_line = vim.api.nvim_get_current_line()
-  local pattern = "\\(.\\+;\\)\\(R\\?(\\?1\\?\\!\\?\\|A\\?(\\?1\\?\\!\\?\\|PC\\?(\\?1\\?\\!\\?\\)\\(.\\+\\)\\(,\\|-\\)\\(.*\\)"
-  local new_cols = vim.fn.substitute(new_line, pattern, "\\3", "")
-  return new_cols
-end
-
-local function replaceOld()
-  vim.fn.search('^TABLE', 'b')
-  vim.fn.search('^R &IN2BASE==', 'W')
-  local start_line = vim.fn.getcurpos()[2] + 1
-  vim.api.nvim_win_set_cursor(0, { start_line, 0 })
-  local end_line = vim.fn.search('\\*\\n', 'W')
-  local lines = vim.api.nvim_buf_get_lines(0, start_line - 1, end_line - 1, false)
-  local newText = table.concat(lines, '\n')
-  wholeText = vim.fn.substitute(wholeText, getColumnsFirst, getColumnsRest, '')
-
-end
-
-vim.api.nvim_create_user_command("Series", getColumnsRest, {})
+vim.api.nvim_create_user_command("Series", tableSeries, {})
