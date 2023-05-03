93a5205005bdb735d094dff997b9a95955598087
alexmccray23 <alex.mccray@gmail.com>
2023-05-02 20:11:18 -0400

    Finished the 2pt and 4pt d/s in nets.lua

--- nets.lua
+++ nets.lua
@@ -12,7 +12,7 @@ function M.two()
   vim.api.nvim_win_set_cursor(0, { start_line, 0 })
   local end_line = start_line + 2
   local text = vim.api.nvim_buf_get_lines(0, start_line, end_line, false)
-  for index,line in ipairs(text) do
+  for index, line in ipairs(text) do
     punches[index] = vim.split(line, ';', { plain = true })
     punches[index][1] = vim.fn.substitute(punches[index][1], "^R\\s\\+\\(TOTAL \\)\\?", "", "")
     punches[index][1] = vim.fn.substitute(punches[index][1], "&IN2\\|&UT-", "", "")
@@ -26,6 +26,45 @@ function M.three()
 end
 
 function M.four()
+  local r_row = {}
+  local punches = {}
+  local newText = { "", "", "", }
+  local start_line = vim.fn.getcurpos()[2] - 1
+  vim.api.nvim_win_set_cursor(0, { start_line + 1, 0 })
+  local end_line = start_line + 4
+  local text = vim.api.nvim_buf_get_lines(0, start_line, end_line, false)
+  local pattern = "\\(.*\\)\\(-\\|,\\)\\(\\d\\+\\)\\()\\?\\)"
+  for i, line in ipairs(text) do
+    newText[i] = vim.fn.substitute(line, "^R ", "R   ", "")
+    r_row[i] = vim.split(line, ';', { plain = true })
+    r_row[i][1] = vim.fn.substitute(r_row[i][1], "^R\\s\\+\\(\\S\\+\\s\\)\\?", "", "")
+    r_row[i][1] = vim.fn.substitute(r_row[i][1], "&IN2\\|&UT-", "", "")
+    punches[i] = vim.fn.substitute(r_row[i][2], pattern, "\\3", "")
+  end
+  local pValue1 = vim.fn.substitute(r_row[1][2], pattern, "\\1\\2\\3:" .. punches[2] .. "\\4", "")
+  local pValue2 = vim.fn.substitute(r_row[3][2], pattern, "\\1\\2\\3:" .. punches[4] .. "\\4", "")
+  local pLabel1 = r_row[1][1]
+  local pLabel2 = r_row[3][1]
+  pLabel1 = vim.fn.substitute(pLabel1, '^ALWAYS', 'ALWAYS//SOMETIMES', "")
+  pLabel1 = vim.fn.substitute(pLabel1, '^GREAT DEAL', 'GREAT DEAL//SOME', "")
+  pLabel1 = vim.fn.substitute(pLabel1, '^LOT', 'A LOT//SOME', "")
+  pLabel2 = vim.fn.substitute(pLabel2, '^RARELY', 'RARELY//NEVER', "")
+  pLabel2 = vim.fn.substitute(pLabel2, '^TOO|^VERY', 'NOT', "")
+  pLabel2 = vim.fn.substitute(pLabel2, '^MUCH', 'NOT MUCH//NOTHING', "")
+  pLabel2 = vim.fn.substitute(pLabel2, '^THAT', 'NOT', "")
+  pLabel2 = vim.fn.substitute(pLabel2, '^LITTLE', 'LITTLE//NOT AT ALL', "")
+  if pLabel2 == 'NOT DESCRIBE WELL' then
+    pLabel1 = vim.fn.substitute(pLabel1, '^VERY', 'DESCRIBES', "")
+    pLabel2 = vim.fn.substitute(pLabel2, '^NOT', "DOESN'T", "")
+  end
+
+  table.insert(newText, 1, "R &IN2**D//S (" .. pLabel1 .. " - " .. pLabel2 .. ");NONE;EX(RD1-RD2) NOSZR")
+  table.insert(newText, 2, "R TOTAL " .. pLabel1 .. "&UT-;" .. pValue1)
+  table.insert(newText, 3, "R TOTAL " .. pLabel2 .. "&UT-;" .. pValue2)
+
+  local nline = vim.fn.line('.')
+  vim.api.nvim_buf_set_lines(0, nline - 1, nline + 3, false, newText)
+  vim.api.nvim_win_set_cursor(0, { nline + 1, 0 })
 end
 
 function M.five()
