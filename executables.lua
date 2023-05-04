cd712bca03007ec537207ea06677ea271e4d29c7
alexmccray23 <alex.mccray@gmail.com>
2023-05-03 21:28:14 -0400

    Finished the T1000 setup in executables.lua

--- executables.lua
+++ executables.lua
@@ -1,4 +1,4 @@
--- Neovim plugin for adding excecutable tablew (900-1000+) to Uncle tables
+-- Neovim plugin for adding excecutable tables (500-1000+) to Uncle E file
 
 local M = {}
 
@@ -62,10 +62,8 @@ end
 
 function M.banner(arg)
   --local footer = ""
-  local footer = "F &CP P U B L I C   O P I N I O N   S T R A T E G I E S"
-  if arg == nil then
-    return
-  elseif arg == 1 then
+  local footer = nil
+  if arg == 1 then
     footer = "F &CP P U B L I C   O P I N I O N   S T R A T E G I E S"
   elseif arg == 2 then
     footer = "F &CP HART RESEARCH ASSOCIATES//PUBLIC OPINION STRATEGIES"
@@ -92,6 +90,72 @@ function M.banner(arg)
 end
 
 function M.stubExec()
+  local tableNum = {}
+  local title = vim.fn.input("\nHeader title\n")
+  local date = vim.fn.input("\nField dates (year will be included)\n")
+  --TODO: source year from filepath
+  local year = "2023"
+  local project = vim.fn.input("\nProject number\n")
+  if title == "" or date == "" or year == "" or project == "" then
+    return
+  end
+  local eFile = vim.fn.readfile(vim.fn.expand("%:p"))
+
+  local index = 1
+  for _, value in ipairs(eFile) do
+    if value:match("^TABLE %d+") then
+      tableNum[index] = value:gsub("^TABLE (%d+)", "%1")
+      tableNum[index] = vim.fn.str2nr(tableNum[index])
+      index = index + 1
+    end
+  end
+  local end1 = nil
+  local start2 = nil
+  local end2 = nil
+  local range
+  for i, value in ipairs(tableNum) do
+    if value == 100 or value == 101 then
+      if tableNum[i - 1] < value - 1 then
+        end1 = tableNum[i - 1]
+        start2 = value
+      end
+    end
+    if value > 100 and end1 == nil then
+      if tableNum[i] - tableNum[i - 1] > 1 then
+        end1 = tableNum[i - 1]
+      end
+    end
+    if value == 500 then
+      for j = i - 1, 1, -1 do
+        if tableNum[j] < 200 then
+          end2 = tableNum[j]
+          break
+        end
+      end
+    end
+  end
+  if start2 == nil then
+    range = end1
+  else
+    range = end1 .. " " .. start2 .. " TH " .. end2
+  end
+
+  local text = {
+    "TABLE 1000",
+    "X ENTER NOADD",
+    "X SET PLINE 175",
+    "X SET PAGE 82",
+    "X WEIGHT UNWEIGHT",
+    "X JOBT '&CP " .. title .. " // " .. project .. " // " .. date .. ", " .. year .. " &RJ PAGE &PS  '",
+    "X SET TITLE '&CP STUB TABLES'",
+    "X SET OUT FILE '" .. project .. ".STB' OFF",
+    "X RUN 1 TH " .. range .. " B 900 PAGE 1 OFF",
+    "X SET TITLE OFF",
+    "X SET OUT FILE '" .. project .. ".STB' CLOSE ",
+    "*" }
+  local nline = vim.fn.line('.')
+  vim.api.nvim_buf_set_lines(0, nline, nline, false, text)
+  vim.api.nvim_win_set_cursor(0, { nline + 12, 0 })
 end
 
 function M.testExec()
@@ -146,7 +210,7 @@ function M.submenu(arg)
   end
   local input = tonumber(vim.fn.input("Select a submenu option: "))
   if input and input >= 1 and input <= #sub_choices then
-    sub_choices[input].func()
+    sub_choices[input].func(input)
   else
     print("Invalid selection")
   end
