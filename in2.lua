af24defb6fe13d9c776ae4e0f55a1a8e6ecf3389
alexmccray23 <alex.mccray@gmail.com>
2023-05-10 22:06:23 -0400

    Patched issue with in2.lua

--- in2.lua
+++ in2.lua
@@ -4,7 +4,7 @@
 local function indent()
   local copy = vim.api.nvim_get_current_line()
   local string = vim.split(copy, ";", { plain = true })[1]
-  string = vim.fn.substitute(string, "^R \\|\\&&UT-", "", "g")
+  string = vim.fn.substitute(string, "^R\\s\\+\\|\\&&UT-", "", "g")
   string = vim.fn.substitute(string, "\\/\\/", "/", "g")
   if #string > 26 then
     if copy:match("%&UT-") then
@@ -13,10 +13,13 @@ local function indent()
       elseif string:gmatch("D/S") and #string % 26 > 0 and #string % 26 < 3 then
         copy = copy:gsub(" D//S", "/D//S")
       end
-    elseif #string > 27 then
-      local array = vim.split(string, " +", { plain = true, trimempty = true })
-      local new_string = "&IN2" .. array[1]
-        copy = copy:gsub(array[1], new_string)
+    elseif #string > 27 and string:sub(1,4) ~= "&IN2" then
+      local pattern = "^(R%s+)(.*)"
+      if string:match("^%d") then
+        copy = copy:gsub(pattern, "%1&IN2 %2", 1)
+      else
+        copy = copy:gsub(pattern, "%1&IN2%2", 1)
+      end
     end
   end
   vim.api.nvim_set_current_line(copy)
