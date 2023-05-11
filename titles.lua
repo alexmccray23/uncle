0ce0ed7b1643c8b36358fe00ab81670a02e6a216
alexmccray23 <alex.mccray@gmail.com>
2023-05-11 14:40:54 -0600

    Some minor changes to titles.lua

--- titles.lua
+++ titles.lua
@@ -2,12 +2,14 @@
 
 local M = {}
 
-function M.copyTable()
-  vim.fn.search('^Q ', 'b')
-  local start_line = vim.fn.getcurpos()[2]
-  vim.api.nvim_win_set_cursor(0, { start_line, 0 })
-  local end_line = vim.fn.search('\\*\\n', 'W')
-  local text = vim.api.nvim_buf_get_lines(0, start_line - 1, end_line - 1, false)
-  return { text, start_line, end_line }
+function M.titleCopy()
+  local titleRowPos = vim.fn.line('.')
+  local text = vim.api.nvim_buf_get_lines(0, titleRowPos - 1, titleRowPos, false)
+  for _, v in ipairs(text) do
+    print(v)
+  end
 end
 
+vim.api.nvim_create_user_command("Titles", M.titleCopy, {})
+
+return M
