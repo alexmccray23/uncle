c58eb69748869396bc995d21573c774ba0b8ca63
alexmccray23 <alex.mccray@gmail.com>
2023-05-22 23:02:52 -0400

    Added 'SELECT CASE' QB snippet to my_snippets.lua

--- my_snippets.lua
+++ my_snippets.lua
@@ -1,45 +1,46 @@
 local ls = require("luasnip")
-local snip = ls.snippet
-local sn = ls.snippet_node
-local insert = ls.insert_node
-local text = ls.text_node
-local choice = ls.choice_node
+local s = ls.snippet
+local i = ls.insert_node
+local t = ls.text_node
+local c = ls.choice_node
+local fmt = require("luasnip.extras.fmt").fmt
 
-ls.add_snippets("all", {
-  snip("ds", {
-    text "R &IN2**D//S (",
-    insert(1, "PUNCH1"),
-    text " - ",
-    insert(2, "PUNCH1"),
-    text ");NONE;EX(RD",
-    insert(3, "1"),
-    text "-RD",
-    insert(0, "2"),
-    text { ") NOSZR", "R" }
+
+ls.add_snippets("eiffel", {
+  s("ds", {
+    t "R &IN2**D//S (",
+    i(1, "PUNCH1"),
+    t " - ",
+    i(2, "PUNCH1"),
+    t ");NONE;EX(RD",
+    i(3, "1"),
+    t "-RD",
+    i(0, "2"),
+    t { ") NOSZR", "R" }
   }),
-  snip("select case", {
-    text "SELECT CASE ",
-    choice(1, {
-      text "VAL(MID$(A$, ",
-      sn(nil, {insert(1, "COL"), text "MID$(A$, " }),
-    })
-  })
 })
 
-local function parseLayout()
-  Data = {}
-  local layDir = vim.split(vim.fn.expand("%:p"), "/", { plain = true })
-  table.remove(layDir, #layDir)
-  local fullPath = table.concat(layDir, "/") .. "/*.[Ll][Aa][Yy]"
-  local temp = vim.fn.glob(fullPath, false, true)
-  local layout = vim.fn.readfile(temp[#temp])
-  for _, value in ipairs(layout) do
-    local column = vim.split(value, ' +', { plain = false, trimempty = true })
-    Data[column[1]] = {
-      startCol = tonumber(column[2]),
-      endCol = tonumber(column[3]),
-      nfield = tonumber(column[5]),
-      wfield = tonumber(column[6])
-    }
-  end
-end
+
+ls.add_snippets("basic", {
+  s("select case",
+    c(1, {
+      fmt([[
+          SELECT CASE VAL(MID$(A$, {}, {}))
+            CASE {}: MID$(A$, {}, {}) = "{}"
+          END SELECT
+        ]],
+        {
+          i(1), i(2), i(3), i(4), i(5), i(6)
+        }),
+      fmt([[
+          SELECT CASE MID$(A$, {}, {})
+            CASE "{}": MID$(A$, {}, {}) = "{}"
+          END SELECT
+        ]],
+        {
+          i(1), i(2), i(3), i(4), i(5), i(6)
+        }),
+    })
+  )
+})
+
