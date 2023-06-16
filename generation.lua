1df4fea6e63fe36007a54cb20ec2cf4d47e3311c
alexmccray23 <alex.mccray@gmail.com>
2023-06-15 23:15:31 -0400

    Refactored generation.lua

--- generation.lua
+++ generation.lua
@@ -31,66 +31,26 @@ function M.standard()
   local qage = M.getColumns()['QAGE']
   local agrp = M.getColumns()['AGEGROUP']
   local year = vim.fn.expand("%:p"):gsub(".*kdata/(%d+).*", "%1")
+  local labels = { "GEN Z", "MILLENIAL", "GEN X", "BABY BOOMERS", "SILENT GEN", "GI GEN" }
+  local start_years = { 1997, 1981, 1965, 1946, 1928, 1901 }
+  local end_years = { 2012, 1996, 1980, 1964, 1945, 1927 }
 
-  local genz_start = 18 --Will need to be updated when applicable (~2030)
-  local genz_end = year - 1997
-  local genz_range = string.format("R(1!%s:%s,%s:%s)", qage['startCol'], qage['endCol'], genz_start, genz_end)
-  local genz_agegroup = ""
-  if genz_start <= 18 and genz_end >= 24 then genz_agegroup = " OR 1!" .. agrp['startCol'] .. "-1" end
-  if genz_start <= 25 and genz_end >= 34 then genz_agegroup = " OR 1!" .. agrp['startCol'] .. "-2" end
-  if genz_start <= 35 and genz_end >= 44 then genz_agegroup = " OR 1!" .. agrp['startCol'] .. "-3" end
-  if genz_start <= 45 and genz_end >= 54 then genz_agegroup = " OR 1!" .. agrp['startCol'] .. "-4" end
-  if genz_start <= 55 and genz_end >= 64 then genz_agegroup = " OR 1!" .. agrp['startCol'] .. "-5" end
-  table.insert(text, "C &CEGEN Z;" .. genz_range .. genz_agegroup)
+  for i = 1, #labels do
+    local start_age = year - end_years[i]
+    local end_age = year - start_years[i]
+    local range = string.format("R(1!%s:%s,%s:%s)", qage['startCol'], qage['endCol'], start_age, end_age)
+    local agegroup = string.format(" OR 1!%s", agrp['startCol'])
+    local grp = ""
 
-  local mill_start = year - 1996
-  local mill_end = year - 1981
-  local mill_range = string.format("R(1!%s:%s,%s:%s)", qage['startCol'], qage['endCol'], mill_start, mill_end)
-  local mill_agegroup = ""
-  if mill_start <= 18 and mill_end >= 24 then mill_agegroup = " OR 1!" .. agrp['startCol'] .. "-1" end
-  if mill_start <= 25 and mill_end >= 34 then mill_agegroup = " OR 1!" .. agrp['startCol'] .. "-2" end
-  if mill_start <= 35 and mill_end >= 44 then mill_agegroup = " OR 1!" .. agrp['startCol'] .. "-3" end
-  if mill_start <= 45 and mill_end >= 54 then mill_agegroup = " OR 1!" .. agrp['startCol'] .. "-4" end
-  if mill_start <= 55 and mill_end >= 64 then mill_agegroup = " OR 1!" .. agrp['startCol'] .. "-5" end
-  table.insert(text, "C &CEMILL=ENIAL;" .. mill_range .. mill_agegroup)
+    if start_age <= 18 and end_age >= 24 then grp = "-1" end
 
-  local genx_start = year - 1980
-  local genx_end = year - 1965
-  local genx_range = string.format("R(1!%s:%s,%s:%s)", qage['startCol'], qage['endCol'], genx_start, genx_end)
-  local genx_agegroup = ""
-  if genx_start <= 18 and genx_end >= 24 then genx_agegroup = " OR 1!" .. agrp['startCol'] .. "-1" end
-  if genx_start <= 25 and genx_end >= 34 then genx_agegroup = " OR 1!" .. agrp['startCol'] .. "-2" end
-  if genx_start <= 35 and genx_end >= 44 then genx_agegroup = " OR 1!" .. agrp['startCol'] .. "-3" end
-  if genx_start <= 45 and genx_end >= 54 then genx_agegroup = " OR 1!" .. agrp['startCol'] .. "-4" end
-  if genx_start <= 55 and genx_end >= 64 then genx_agegroup = " OR 1!" .. agrp['startCol'] .. "-5" end
-  table.insert(text, "C &CEGEN X;" .. genx_range .. genx_agegroup)
+    for j = 25, 55, 10 do
+      if start_age <= j and end_age >= (j + 9) then grp = string.format("-%d", (j - 5) / 10) end
+    end
 
-  local boom_start = year - 1964
-  local boom_end = year - 1946
-  local boom_range = string.format("R(1!%s:%s,%s:%s)", qage['startCol'], qage['endCol'], boom_start, boom_end)
-  local boom_agegroup = ""
-  if boom_start <= 18 and boom_end >= 24 then boom_agegroup = " OR 1!" .. agrp['startCol'] .. "-1" end
-  if boom_start <= 25 and boom_end >= 34 then boom_agegroup = " OR 1!" .. agrp['startCol'] .. "-2" end
-  if boom_start <= 35 and boom_end >= 44 then boom_agegroup = " OR 1!" .. agrp['startCol'] .. "-3" end
-  if boom_start <= 45 and boom_end >= 54 then boom_agegroup = " OR 1!" .. agrp['startCol'] .. "-4" end
-  if boom_start <= 55 and boom_end >= 64 then boom_agegroup = " OR 1!" .. agrp['startCol'] .. "-5" end
-  table.insert(text, "C &CEBABY BOOM- ERS;" .. boom_range .. boom_agegroup)
-
-  local silent_start = year - 1945
-  local silent_end = year - 1928
-  local silent_range = string.format("R(1!%s:%s,%s:%s)", qage['startCol'], qage['endCol'], silent_start, silent_end)
-  local silent_agegroup = ""
-  if silent_start <= 18 and silent_end >= 24 then silent_agegroup = " OR 1!" .. agrp['startCol'] .. "-1" end
-  if silent_start <= 25 and silent_end >= 34 then silent_agegroup = " OR 1!" .. agrp['startCol'] .. "-2" end
-  if silent_start <= 35 and silent_end >= 44 then silent_agegroup = " OR 1!" .. agrp['startCol'] .. "-3" end
-  if silent_start <= 45 and silent_end >= 54 then silent_agegroup = " OR 1!" .. agrp['startCol'] .. "-4" end
-  if silent_start <= 55 and silent_end >= 64 then silent_agegroup = " OR 1!" .. agrp['startCol'] .. "-5" end
-  table.insert(text, "C &CESILENT GEN;" .. silent_range .. silent_agegroup)
-
-  local gi_start = year - 1927
-  local gi_end = year - 1901
-  local gi_range = string.format("R(1!%s:%s,%s:%s)", qage['startCol'], qage['endCol'], gi_start, gi_end)
-  table.insert(text, "C &CEGI GEN;" .. gi_range)
+    if #grp > 0 then agegroup = agegroup .. grp else agegroup = "" end
+    table.insert(text, "C &CE" .. labels[i] .. ";" .. range .. agegroup)
+  end
 
   local nline = vim.fn.line('.')
   vim.api.nvim_buf_set_lines(0, nline - 1, nline - 1, false, text)
@@ -102,127 +62,26 @@ function M.nbc()
   local qage = M.getColumns()['AGE']
   local agrp = M.getColumns()['Q2A']
   local year = vim.fn.expand("%:p"):gsub(".*kdata/(%d+).*", "%1")
+  local labels = { "GEN Z", "MILLENIAL", "GEN X", "BABY BOOMERS", "SILENT GEN", "GI GEN" }
+  local start_years = { 1997, 1981, 1965, 1946, 1928, 1901 }
+  local end_years = { 2012, 1996, 1980, 1964, 1945, 1927 }
 
-  local genz_start = 18 --Will need to be updated when applicable (~2030)
-  local genz_end = year - 1997
-  local genz_range = string.format("R(1!%s:%s,%s:%s)", qage['startCol'], qage['endCol'], genz_start, genz_end)
-  local genz_agegroup = string.format(" OR R(1!%s:%s", agrp['startCol'], agrp['endCol'])
-  local genz_grp = ""
-  if genz_start <= 18 and genz_end >= 24 then genz_grp = genz_grp .. ",01" end
-  if genz_start <= 25 and genz_end >= 29 then genz_grp = genz_grp .. ",02" end
-  if genz_start <= 30 and genz_end >= 34 then genz_grp = genz_grp .. ",03" end
-  if genz_start <= 35 and genz_end >= 39 then genz_grp = genz_grp .. ",04" end
-  if genz_start <= 40 and genz_end >= 44 then genz_grp = genz_grp .. ",05" end
-  if genz_start <= 45 and genz_end >= 49 then genz_grp = genz_grp .. ",06" end
-  if genz_start <= 50 and genz_end >= 54 then genz_grp = genz_grp .. ",07" end
-  if genz_start <= 55 and genz_end >= 59 then genz_grp = genz_grp .. ",08" end
-  if genz_start <= 60 and genz_end >= 64 then genz_grp = genz_grp .. ",09" end
-  if genz_start <= 65 and genz_end >= 69 then genz_grp = genz_grp .. ",10" end
-  if genz_start <= 70 and genz_end >= 74 then genz_grp = genz_grp .. ",11" end
-  if genz_start <= 75 and genz_end >= 99 then genz_grp = genz_grp .. ",12" end
-  if #genz_grp > 0 then genz_agegroup = genz_agegroup .. genz_grp .. ")" else genz_agegroup = "" end
-  table.insert(text, "C &CEGEN Z;" .. genz_range .. genz_agegroup)
+  for i = 1, #labels do
+    local start_age = year - end_years[i]
+    local end_age = year - start_years[i]
+    local range = string.format("R(1!%s:%s,%s:%s)", qage['startCol'], qage['endCol'], start_age, end_age)
+    local agegroup = string.format(" OR R(1!%s:%s", agrp['startCol'], agrp['endCol'])
+    local grp = ""
 
-  local mill_start = year - 1996
-  local mill_end = year - 1981
-  local mill_range = string.format("R(1!%s:%s,%s:%s)", qage['startCol'], qage['endCol'], mill_start, mill_end)
-  local mill_agegroup = string.format(" OR R(1!%s:%s", agrp['startCol'], agrp['endCol'])
-  local mill_grp = ""
-  if mill_start <= 18 and mill_end >= 24 then mill_grp = mill_grp .. ",01" end
-  if mill_start <= 25 and mill_end >= 29 then mill_grp = mill_grp .. ",02" end
-  if mill_start <= 30 and mill_end >= 34 then mill_grp = mill_grp .. ",03" end
-  if mill_start <= 35 and mill_end >= 39 then mill_grp = mill_grp .. ",04" end
-  if mill_start <= 40 and mill_end >= 44 then mill_grp = mill_grp .. ",05" end
-  if mill_start <= 45 and mill_end >= 49 then mill_grp = mill_grp .. ",06" end
-  if mill_start <= 50 and mill_end >= 54 then mill_grp = mill_grp .. ",07" end
-  if mill_start <= 55 and mill_end >= 59 then mill_grp = mill_grp .. ",08" end
-  if mill_start <= 60 and mill_end >= 64 then mill_grp = mill_grp .. ",09" end
-  if mill_start <= 65 and mill_end >= 69 then mill_grp = mill_grp .. ",10" end
-  if mill_start <= 70 and mill_end >= 74 then mill_grp = mill_grp .. ",11" end
-  if mill_start <= 75 and mill_end >= 99 then mill_grp = mill_grp .. ",12" end
-  if #mill_grp > 0 then mill_agegroup = mill_agegroup .. mill_grp .. ")" else mill_agegroup = "" end
-  table.insert(text, "C &CEMILL=ENIAL;" .. mill_range .. mill_agegroup)
+    if start_age <= 18 and end_age >= 24 then grp = ",01" end
 
-  local genx_start = year - 1980
-  local genx_end = year - 1965
-  local genx_range = string.format("R(1!%s:%s,%s:%s)", qage['startCol'], qage['endCol'], genx_start, genx_end)
-  local genx_agegroup = string.format(" OR R(1!%s:%s", agrp['startCol'], agrp['endCol'])
-  local genx_grp = ""
-  if genx_start <= 18 and genx_end >= 24 then genx_grp = genx_grp .. ",01" end
-  if genx_start <= 25 and genx_end >= 29 then genx_grp = genx_grp .. ",02" end
-  if genx_start <= 30 and genx_end >= 34 then genx_grp = genx_grp .. ",03" end
-  if genx_start <= 35 and genx_end >= 39 then genx_grp = genx_grp .. ",04" end
-  if genx_start <= 40 and genx_end >= 44 then genx_grp = genx_grp .. ",05" end
-  if genx_start <= 45 and genx_end >= 49 then genx_grp = genx_grp .. ",06" end
-  if genx_start <= 50 and genx_end >= 54 then genx_grp = genx_grp .. ",07" end
-  if genx_start <= 55 and genx_end >= 59 then genx_grp = genx_grp .. ",08" end
-  if genx_start <= 60 and genx_end >= 64 then genx_grp = genx_grp .. ",09" end
-  if genx_start <= 65 and genx_end >= 69 then genx_grp = genx_grp .. ",10" end
-  if genx_start <= 70 and genx_end >= 74 then genx_grp = genx_grp .. ",11" end
-  if genx_start <= 75 and genx_end >= 99 then genx_grp = genx_grp .. ",12" end
-  if #genx_grp > 0 then genx_agegroup = genx_agegroup .. genx_grp .. ")" else genx_agegroup = "" end
-  table.insert(text, "C &CEGEN X;" .. genx_range .. genx_agegroup)
+    for j = 25, 70, 5 do
+      if start_age <= j and end_age >= (j + 4) then grp = grp .. string.format(",%02d", (j - 15) / 5) end
+    end
 
-  local boom_start = year - 1964
-  local boom_end = year - 1946
-  local boom_range = string.format("R(1!%s:%s,%s:%s)", qage['startCol'], qage['endCol'], boom_start, boom_end)
-  local boom_agegroup = string.format(" OR R(1!%s:%s", agrp['startCol'], agrp['endCol'])
-  local boom_grp = ""
-  if boom_start <= 18 and boom_end >= 24 then boom_grp = boom_grp .. ",01" end
-  if boom_start <= 25 and boom_end >= 29 then boom_grp = boom_grp .. ",02" end
-  if boom_start <= 30 and boom_end >= 34 then boom_grp = boom_grp .. ",03" end
-  if boom_start <= 35 and boom_end >= 39 then boom_grp = boom_grp .. ",04" end
-  if boom_start <= 40 and boom_end >= 44 then boom_grp = boom_grp .. ",05" end
-  if boom_start <= 45 and boom_end >= 49 then boom_grp = boom_grp .. ",06" end
-  if boom_start <= 50 and boom_end >= 54 then boom_grp = boom_grp .. ",07" end
-  if boom_start <= 55 and boom_end >= 59 then boom_grp = boom_grp .. ",08" end
-  if boom_start <= 60 and boom_end >= 64 then boom_grp = boom_grp .. ",09" end
-  if boom_start <= 65 and boom_end >= 69 then boom_grp = boom_grp .. ",10" end
-  if boom_start <= 70 and boom_end >= 74 then boom_grp = boom_grp .. ",11" end
-  if boom_start <= 75 and boom_end >= 99 then boom_grp = boom_grp .. ",12" end
-  if #boom_grp > 0 then boom_agegroup = boom_agegroup .. boom_grp .. ")" else boom_agegroup = "" end
-  table.insert(text, "C &CEBABY BOOM- ERS;" .. boom_range .. boom_agegroup)
-
-
-  local silent_start = year - 1945
-  local silent_end = year - 1928
-  local silent_range = string.format("R(1!%s:%s,%s:%s)", qage['startCol'], qage['endCol'], silent_start, silent_end)
-  local silent_agegroup = string.format(" OR R(1!%s:%s", agrp['startCol'], agrp['endCol'])
-  local silent_grp = ""
-  if silent_start <= 18 and silent_end >= 24 then silent_grp = silent_grp .. ",01" end
-  if silent_start <= 25 and silent_end >= 29 then silent_grp = silent_grp .. ",02" end
-  if silent_start <= 30 and silent_end >= 34 then silent_grp = silent_grp .. ",03" end
-  if silent_start <= 35 and silent_end >= 39 then silent_grp = silent_grp .. ",04" end
-  if silent_start <= 40 and silent_end >= 44 then silent_grp = silent_grp .. ",05" end
-  if silent_start <= 45 and silent_end >= 49 then silent_grp = silent_grp .. ",06" end
-  if silent_start <= 50 and silent_end >= 54 then silent_grp = silent_grp .. ",07" end
-  if silent_start <= 55 and silent_end >= 59 then silent_grp = silent_grp .. ",08" end
-  if silent_start <= 60 and silent_end >= 64 then silent_grp = silent_grp .. ",09" end
-  if silent_start <= 65 and silent_end >= 69 then silent_grp = silent_grp .. ",10" end
-  if silent_start <= 70 and silent_end >= 74 then silent_grp = silent_grp .. ",11" end
-  if silent_start <= 75 and silent_end >= 99 then silent_grp = silent_grp .. ",12" end
-  if #silent_grp > 0 then silent_agegroup = silent_agegroup .. silent_grp .. ")" else silent_agegroup = "" end
-  table.insert(text, "C &CESILENT GEN;" .. silent_range .. silent_agegroup)
-
-  local gi_start = year - 1927
-  local gi_end = year - 1901
-  local gi_range = string.format("R(1!%s:%s,%s:%s)", qage['startCol'], qage['endCol'], gi_start, gi_end)
-  local gi_agegroup = string.format(" OR R(1!%s:%s", agrp['startCol'], agrp['endCol'])
-  local gi_grp = ""
-  if gi_start <= 18 and gi_end >= 24 then gi_grp = gi_grp .. ",01" end
-  if gi_start <= 25 and gi_end >= 29 then gi_grp = gi_grp .. ",02" end
-  if gi_start <= 30 and gi_end >= 34 then gi_grp = gi_grp .. ",03" end
-  if gi_start <= 35 and gi_end >= 39 then gi_grp = gi_grp .. ",04" end
-  if gi_start <= 40 and gi_end >= 44 then gi_grp = gi_grp .. ",05" end
-  if gi_start <= 45 and gi_end >= 49 then gi_grp = gi_grp .. ",06" end
-  if gi_start <= 50 and gi_end >= 54 then gi_grp = gi_grp .. ",07" end
-  if gi_start <= 55 and gi_end >= 59 then gi_grp = gi_grp .. ",08" end
-  if gi_start <= 60 and gi_end >= 64 then gi_grp = gi_grp .. ",09" end
-  if gi_start <= 65 and gi_end >= 69 then gi_grp = gi_grp .. ",10" end
-  if gi_start <= 70 and gi_end >= 74 then gi_grp = gi_grp .. ",11" end
-  if gi_start <= 75 and gi_end >= 99 then gi_grp = gi_grp .. ",12" end
-  if #gi_grp > 0 then gi_agegroup = gi_agegroup .. gi_grp .. ")" else gi_agegroup = "" end
-  table.insert(text, "C &CEGI GEN;" .. gi_range .. gi_agegroup)
+    if #grp > 0 then agegroup = agegroup .. grp .. ")" else agegroup = "" end
+    table.insert(text, "C &CE" .. labels[i] .. ";" .. range .. agegroup)
+  end
 
   local nline = vim.fn.line('.')
   vim.api.nvim_buf_set_lines(0, nline - 1, nline - 1, false, text)
