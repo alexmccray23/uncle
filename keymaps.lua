b8081d75a5a1831d66032f567c3b22cba2c06718
alexmccray23 <alex.mccray@gmail.com>
2023-07-02 08:29:42 -0400

    minor keymap changes

--- keymaps.lua
+++ keymaps.lua
@@ -1,11 +1,11 @@
 
 local keymap = vim.api.nvim_set_keymap
-keymap('n', '<leader>in', ':lua require("user.uncle.indent").indent()<CR>', { noremap = true, silent = true, desc = "Uncle: Indent" })
 keymap('n', '<leader>ds', ':DiffScore<CR>', { noremap = true, silent = true, desc = "Uncle: [D]ifference [S]core" })
+keymap('n', '<leader>in', ':lua require("user.uncle.indent").indent()<CR>', { noremap = true, silent = true, desc = "Uncle: Indent" })
 keymap('n', '<leader>k', ':lua require("user.uncle.uncle_syntax").uncleSyntax()<CR>', { noremap = true, silent = true, desc = "Uncle: QX-1 syntax" })
 keymap('n', '<leader>j', ':lua require("user.uncle.uncle_syntax").combFix()<CR>', { noremap = true, silent = true, desc = "Uncle: QX:1 syntax" })
 keymap('n', '<leader>ii', ':lua require("user.uncle.in2").indent()<CR>', { noremap = true, silent = true, desc = "Uncle: &IN2" })
-keymap('n', '<leader>bc', ':lua require("user.uncle.banner_column").bannerColumn()<CR>', { noremap = true, silent = true, desc = "Uncle: Insert banner columns" })
+keymap('n', '<leader>bc', ':lua require("user.uncle.banner_column").bannerColumn()<CR>', { noremap = true, silent = true, desc = "Uncle: Insert [B]anner [C]olumns" })
 keymap('n', '<leader>rr', ':lua require("user.uncle.r_row").rRow()<CR>', { noremap = true, silent = true, desc = "Uncle: Insert R Row" })
 keymap('n', '<leader>u', ':lua require("user.uncle.underline").underline()<CR>', { noremap = true, silent = true, desc = "Uncle: Underline" })
 keymap('n', '<leader>p', ':lua require("user.uncle.norank").noRank()<CR>', { noremap = true, silent = true, desc = "Uncle: No rank" })
