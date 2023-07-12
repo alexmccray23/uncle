01e14065d5c7d2434c1208714b3c39d25195ff17
alexmccray23 <alex.mccray@gmail.com>
2023-07-12 13:05:31 -0600

    Updated layout.lua regex

--- layout.lua
+++ layout.lua
@@ -1,7 +1,7 @@
 -- Layout reformatting plugin 
 function LayoutReformat()
   vim.cmd([[normal gg5dd
-            %s/^\s\+\(\S\+\s\+\)\(SYS VAR\|SELECT\|NUM\|VARIABLE\)\(\s\+\)\(\S\+\s\+\)\(\S\+\s\+\)\(\S\+\s\+\)\?\(\S\+\s\+\)\(\d\+\s\+\)\(\d\+\)/ \1\8\9 \2\3\4\5\6\7/g
+            %s/^\s\+\(\S\+\s\+\)\(SYS VAR\|SELECT\|NUM\|VARIABLE\)\(\s\+\S\+\s\+\S\+\s\+\)\(\S\+\s\+\)\?\(\S\+\s\+\)\(\d\+\s\+\)\(\d\+\)/ \1\6\7 \2\3\4\5/g
             normal gg]])
 end
 vim.api.nvim_create_user_command("Layout", LayoutReformat, {})
