7f4eab45020dd85514352ecf0c5e070f0df83cf6
alexmccray23 <alex.mccray@gmail.com>
2025-02-19 05:35:52 -0700

    Fix minor bug in skip.lua

--- skip.lua
+++ skip.lua
@@ -3,10 +3,10 @@
 local M = {}
 
 function M.copyTable()
-  vim.fn.search('^Q ', 'b')
+  vim.fn.search("^Q ", "b")
   local start_line = vim.fn.getcurpos()[2]
   vim.api.nvim_win_set_cursor(0, { start_line, 0 })
-  local end_line = vim.fn.search('\\*\\n', 'W')
+  local end_line = vim.fn.search("\\*\\n", "W")
   local text = vim.api.nvim_buf_get_lines(0, start_line - 1, end_line - 1, false)
   return { text, start_line, end_line }
 end
@@ -16,10 +16,10 @@ function M.parseTable()
   local skip = {}
   local qual = ""
   for _, line in ipairs(text) do
-    if line:match("^Q .") then
+    if line:match "^Q ." then
       qual = line:gsub("^Q (.*)", "%1")
-    elseif line:match("R &IN2BASE==") then
-      line = line:gsub("ALL", qual)
+    elseif line:match "R &IN2BASE==" then
+      line = line:gsub(";ALL;", ";" .. qual .. ";")
       table.insert(skip, line)
     else
       local array = vim.split(line, ";", { plain = true })
@@ -34,7 +34,7 @@ function M.parseTable()
   table.insert(skip, "R -------------;NULL")
   table.insert(skip, "R &IN2BASE==TOTAL SAMPLE;ALL;HP NOVP")
   for _, line in ipairs(text) do
-    if line:match("^R ") and not line:match("^R &IN2BASE==") then
+    if line:match "^R " and not line:match "^R &IN2BASE==" then
       table.insert(skip, line)
     end
   end
